434c4d3567ce345c6f9a1ed9091e2082c15fcf0f
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-10-02 20:22:40 +1100

    pseudo code for my 3 blocks

---
+++ dbchat/src/query_execution_and_feedback.py
@@ -0,0 +1,15 @@
+"""
+This blocks includes executing the generated query against the 
+database and provide the feedbacks as needed and also implements 
+error handling mechanisms to handle cases where the user query 
+does not match any data models or contains ambiguous information.
+"""
+
+# Convert natural language response into SQL statement, and query DB
+
+
+# Catch database call errors
+
+
+# Return error messages to agent, or continue
+
