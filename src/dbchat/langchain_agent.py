0e97f0762c33e35a8379a3c5b5fb2da22b72e141
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-10-29 03:07:01 +1100

    moved to src/dbchat

---
+++ src/dbchat/langchain_agent.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from typing import Optional
+
+import pandas as pd
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+
+from Experiments.logger import GitLogger
+
+
+class LangchainAgent:
+    def __init__(self, dataframes, logger: Optional[logging.Logger]=GitLogger):
+        if logger:
+            self.logger = logger
+
+        self.dataframes = dataframes        
+        self.agent = create_pandas_dataframe_agent(
+            ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+            self.dataframes,
+            verbose=True,
+            agent_type=AgentType.OPENAI_FUNCTIONS,
+        )
+
+    def ask(self, processed_prompt):
+        response = agent.run(processed_prompt)
+        if self.logger:
+            self.logger.log(processed_prompt)
+            self.logger.log(response)
+            
+        return response
