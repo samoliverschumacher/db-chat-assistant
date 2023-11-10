8ef55b074fd74501e3f230415f18e9666fa6897b
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-11-10 15:56:17 +1100

    updated test structure

--- src/tests/test_llm.py
+++ src/tests/test_llm.py
@@ -1,31 +1,101 @@
 ################################################################################################
-#           TEST CONTEXT RETRIEVAL
+#           TEST IF RESPONSE CONTAINS THE CONTEXT IT WAS GIVEN
 ################################################################################################
+# Checks if the synthesized response can be deduced from the sources. If not, there is halluciniation.
 
 # Load an example user query
 
 # Initialise the encoder (llm)
 
-# Encode the query
+# Retrieve
+#     Encode the query
 
-# load the array of document vectors
+#     load the array of document vectors
 
-# flatten the array of chunks of documents, to a 1D array of document-chunks
+#     flatten the array of chunks of documents, to a 1D array of document-chunks
 
-# Compare the encoded query to all the document-chunks
+#     Compare the encoded query to all the document-chunks
 
-# identify the most similar document-chunk
+#     identify the most similar document-chunk
 
-# identify the chunk before, and chunk after the most similar document-chunk, if they exist.
+#     identify the chunk before, and chunk after the most similar document-chunk, if they exist.
 
-# Load from the vector database the most similar document-chunk, and the before and after chunks.
+#     Load from the vector database the most similar document-chunk, and the before and after chunks.
+
+# Compose the prompt with the context (document chunks), the original query, and an instruction
+# for the llm to decide if the response could be given using the context information alone.
+
+# Call the llm with the prompt.
+
+# Score the model - "1" if it said there was enough info, "0.5" if partially, and "0" if not.
+
+
+################################################################################################
+#           TEST CONTEXT RETRIEVAL
+################################################################################################
+# Given a query, use a LLM to decide if the context retrieved contains enough to answer the query
+
+# Load an example user query
+
+# Initialise the encoder (llm)
+
+# Retrieve
+#     Encode the query
+
+#     load the array of document vectors
+
+#     flatten the array of chunks of documents, to a 1D array of document-chunks
+
+#     Compare the encoded query to all the document-chunks
+
+#     identify the most similar document-chunk
+
+#     identify the chunk before, and chunk after the most similar document-chunk, if they exist.
+
+#     Load from the vector database the most similar document-chunk, and the before and after chunks.
 
 # Compose the prompt with the context (document chunks), the original query, and an instruction 
 # for the llm to decide if the context information has enough in it to answer the users query.
 
 # Call the llm with the prompt.
 
-# Post-process the response, score the result as "1" if it said there was enough info, "0.5" if partially, and "0" if not.
+# Post-process the response, score the result as "1" if it said there was enough info, "0.5" if partially, 
+# and "0" if not.
+
+
+################################################################################################
+#           TEST IF RESPONSE & SOURCE CONTEXT ANSWERS THE QUERY
+################################################################################################
+# 
+
+# Load an example user query
+
+# Initialise the encoder (llm)
+
+# Retrieve
+#     Encode the query
+
+#     load the array of document vectors
+
+#     flatten the array of chunks of documents, to a 1D array of document-chunks
+
+#     Compare the encoded query to all the document-chunks
+
+#     identify the most similar document-chunk
+
+#     identify the chunk before, and chunk after the most similar document-chunk, if they exist.
+
+#     Load from the vector database the most similar document-chunk, and the before and after chunks.
+
+# Compose the prompt with the context (document chunks), the original query, the synthesized response
+# and an instruction for the llm to decide if the response answers the query.
+
+# Call the llm with the prompt.
+
+# Score the model - "1" if it said there was enough info, "0.5" if partially, and "0" if not.
+
+# Compose the prompt with the context (document chunks), the original query, the synthesized response
+# and an instruction for the llm to decide if each of the context sources contains an answer to the query
 
 
 ################################################################################################
@@ -42,7 +112,20 @@
 
 # Load example user query
 
-# Retrieve the context from the vector database
+# Retrieve
+#     Encode the query
+
+#     load the array of document vectors
+
+#     flatten the array of chunks of documents, to a 1D array of document-chunks
+
+#     Compare the encoded query to all the document-chunks
+
+#     identify the most similar document-chunk
+
+#     identify the chunk before, and chunk after the most similar document-chunk, if they exist.
+
+#     Load from the vector database the most similar document-chunk, and the before and after chunks.
 
 # Compose the prompt with the context
 
@@ -53,3 +136,9 @@
 # Get a score for the user query from the "Judge" LLM
 
 # Save the score and the explanation of the score to file.
+
+
+################################################################################################
+#           TEST SYNTHESIZED RESPONSE AGAINST HUMAN CRAFTED ANSWER - 
+################################################################################################
+
