14231c2dc05b1d063f027bdaaf8354277f5df801
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-11-13 22:54:40 +1100

    implement test to compare human-specified reference with synthesized

--- src/tests/test_llm.py
+++ src/tests/test_llm.py
@@ -109,12 +109,66 @@
 ################################################################################################
 #           TEST SYNTHESIZED RESPONSE AGAINST HUMAN CRAFTED ANSWER - LLM AS JUDGE
 ################################################################################################
+from langchain.embeddings import OllamaEmbeddings
+from langchain.llms.ollama import Ollama as LangchainOllama
+from llama_index.llms import Ollama
+from llama_index import ServiceContext, set_global_service_context
+from llama_index import VectorStoreIndex
+from llama_index import download_loader
+import pandas as pd
+from sqlalchemy import create_engine
+
+from dbchat import ROOT_DIR
+from dbchat.langchain_agent import LangchainAgent
+
+
+DATA_DIR = ROOT_DIR.parent.parent / "data"
+db_path = str(DATA_DIR / "chinook.db")
+test_data_path = ROOT_DIR.parent / "tests/data/inputs/end-to-end.csv"
 
 # Load example user query
+def load_example_queries(test_data_path):
+    test_data = []
+    with open(test_data_path) as f:
+        f.readline()  # Remove header row
+        for row in f.readlines():
+            id, user_query, tables, comment = row.split('|')
+            test_data.append((id, user_query, tables, comment))
+    return test_data
+
+test_data = load_example_queries(test_data_path)
+expected_response = "Berlin made $75.24"
+
+input_query = test_data[0][1]
+expected_tables = test_data[0][2].split(',')
+
+def load_metadata_from_sqllite(db_path):
+    DatabaseReader = download_loader("DatabaseReader")
+
+    engine = create_engine(f"sqlite:///{db_path}")
+    reader = DatabaseReader(engine = engine) # type: ignore
+
+    query = "SELECT DESCRIPTION FROM table_descriptions"
+    document_ids = reader.load_data(query=query)
+    
+    query = "SELECT TABLE_NAME FROM table_descriptions"
+    documents = reader.load_data(query=query)
+    return documents, document_ids
+
+documents, document_ids = load_metadata_from_sqllite(db_path)
+document_map = {doc.id_: docid.text for docid, doc in zip(document_ids, documents)}
 
 # Initialise the encoder (llm)
+llm_orcamini = Ollama(model="orca-mini")
+ollama_emb = OllamaEmbeddings(model="orca-mini") # type: ignore
+ctx = ServiceContext.from_defaults(llm=llm_orcamini, embed_model=ollama_emb)
+set_global_service_context(ctx)
+orcamini_index = VectorStoreIndex.from_documents(documents, service_context=ctx)
+retriever = orcamini_index.as_retriever()
 
 # Retrieve
+nodes = retriever.retrieve(input_query)
+
 #     Encode the query
 
 #     load the array of document vectors
@@ -130,14 +184,43 @@
 #     Load from the vector database the most similar document-chunk, and the before and after chunks.
 
 # Compose the prompt with the context
+print(nodes)
+context_tables = [document_map[node.node.id_] for node in nodes]
+
+# load pandas dataframes from data directory, for each context_table
+frames = []
+for table in context_tables:
+    table_path = DATA_DIR / table
+    df = pd.read_csv(table_path)
+    frames.append(df)
 
 # Get the LLMs response
+agent = LangchainAgent(dataframes=frames, 
+                       llm=LangchainOllama(model="orca-mini")) # type: ignore
+response = agent.ask(input_query)
 
 # Compose a prompt with the original query and the synthesized response for the "Judge" LLM
+prompt_data = f"""
+Human: 
+You are to rate a summarization on the scale of 0-10, with 0 being completely incorrect and 10 being a perfect summzarization of the given text.
+Explain why you give your score.
+Give a bullet point list of major differences between the reference and the summary.
+I will supply a reference text by denoting REF: and the summarization to compare against with SUMMARY:.
+
+REF:
+{expected_response}
+
+SUMMARY:
+{response}
+
+Assistant:"""
 
 # Get a score for the user query from the "Judge" LLM
+llm = Ollama(model="orca-mini")
+evaluation = llm.complete(prompt_data)
 
 # Save the score and the explanation of the score to file.
+print(evaluation)
 
 
 ################################################################################################
