f910e7725cc22c1dd6ce45d69dca3153f787aba5
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-11-29 16:44:24 +1100

    condensed langchain pandas agent into 1 file

--- src/dbchat/pandas_agent.py
+++ src/dbchat/pandas_agent.py
@@ -1,10 +1,58 @@
-from langchain.embeddings import OllamaEmbeddings
-from langchain.chat_models import ChatOpenAI
-from llama_index import ServiceContext, VectorStoreIndex, set_global_service_context
-from numpy import single
+import logging
+import os
+from typing import Optional
+
 import pandas as pd
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import FileCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OllamaEmbeddings
+from langchain.llms.ollama import Ollama as LangchainOllama
+from langchain.schema.language_model import BaseLanguageModel
+from langchain_experimental.agents.agent_toolkits import \
+    create_pandas_dataframe_agent
+from llama_index import (ServiceContext, VectorStoreIndex,
+                         set_global_service_context)
+
 from dbchat import datastore
-from dbchat.langchain_agent import PandasLangchainAgent
+
+# pip install openai
+# pip install langchain-experimental
+# conda install -c conda-forge tabulate
+
+
+class PandasLangchainAgent:
+
+    def __init__(self,
+                 dataframes,
+                 llm: BaseLanguageModel = ChatOpenAI(
+                     temperature=0, model="gpt-3.5-turbo-0613"),
+                 logger: Optional[logging.Logger] = None,
+                 logfile='output.log'):
+        self.logger = logger
+
+        self.dataframes = dataframes
+        self.agent = create_pandas_dataframe_agent(
+            llm=llm,
+            df=self.dataframes,
+            verbose=True,
+            agent_type=AgentType.OPENAI_FUNCTIONS,
+        )
+        self.agent.handle_parsing_errors = True
+        # TODO: design one interface for all loggers. Or make them play nicely together.
+        log_handler = FileCallbackHandler(logfile)
+        self.agent.callbacks = [log_handler]
+
+    def ask(self, processed_prompt):
+        runkwargs = {"input": processed_prompt}
+        response = self.agent.run(**runkwargs)
+        if self.logger is not None:
+            msg = (f"Asking pandas dataframe agent..."
+                   f"{processed_prompt}"
+                   f"{response}")
+            self.logger.log(logging.INFO, msg)
+
+        return response
 
 
 def pandas_langchain_agent(cfg, context_tables):
