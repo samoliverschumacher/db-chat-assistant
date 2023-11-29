cc99dabb5be08f7d02d5d7cffd4f32b3ee27bef7
samoliverschumacher <ssch7766@uni.sydney.edu.au>
2023-11-29 16:51:37 +1100

    reorganised tests, removed some, moved some to evaluation library

--- src/tests/test_validation.py
+++ src/tests/test_validation.py
@@ -7,13 +7,13 @@ from dbchat.validation import load_config
 class TestConfig(unittest.TestCase):
 
     def test_load_valid_config(self):
-        config = load_config('valid_config.yml')
+        config = load_config('inputs/valid_config.yml')
         self.assertIsNotNone(config)
         self.assertEqual(config.appproach, "sql_engine_w_reranking")
 
     def test_load_invalid_config(self):
         with self.assertRaises(ValidationError):
-            config = load_config('invalid_config.yml')
+            config = load_config('inputs/invalid_config.yml')
 
 
 if __name__ == '__main__':
